3192c2fc042bb3643212b21dca7a8882c56bf753
Hans Gaensbauer <hans@gridware.io>
2022-03-31 22:35:18 -0700

    Initial Commit
    Add existing project files to repo.

---
+++ Firmware/main/nmr.h
@@ -0,0 +1,15 @@
+
+
+#ifndef NMR_H_
+#define NMR_H_
+
+#define F_CPU 20000000UL
+
+//Constants for pulse shaping. These are in microseconds
+#define PULSEWIDTH_90 100
+#define PULSE_SPACING 200
+#define PULSETRAIN_DELAY 100
+
+#define NUMPULSES 2000
+
+#endif //NMR_H_
