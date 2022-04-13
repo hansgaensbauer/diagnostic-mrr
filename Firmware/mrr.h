266eb1dcb2252efe57bd30395dbf744cd18e9d21
Hans Gaensbauer <hans@gridware.io>
2022-04-12 17:58:08 -0700

    Update Makefile
    Reorganized project to make flashing calibration firmware cleaner

---
+++ Firmware/mrr.h
@@ -0,0 +1,13 @@
+#ifndef MRR_H_
+#define MRR_H_
+
+#define F_CPU 20000000UL
+
+//Constants for pulse shaping. These are in microseconds
+#define PULSEWIDTH_90 100
+#define PULSE_SPACING 200
+#define PULSETRAIN_DELAY 100
+
+#define NUMPULSES 2000
+
+#endif //MRR_H_
